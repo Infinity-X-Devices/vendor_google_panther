b20156afa317cd8d80a8208d484461b6f7dad513
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2025-03-14 14:59:19 -0400

    panther: drop hbm since we use pixelparts * credits goes to evo team
    Signed-off-by: Anthony Pyrtle <xxpyrtlexx93@gmail.com>

--- panther-vendor.mk
+++ panther-vendor.mk
@@ -551,7 +551,6 @@ PRODUCT_PACKAGES += \
     libmediaadaptor \
     com.google.edgetpu_app_service-V3-ndk \
     com.google.edgetpu_vendor_service-V2-ndk \
-    libhbmsvmanager_jni \
     VZWAPNLib \
     AppDirectedSMSService \
     CarrierSettings_signed_53.0.733201674 \
@@ -577,7 +576,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     OemRilService \
     PixelQualifiedNetworksService \
@@ -604,8 +602,7 @@ PRODUCT_PACKAGES += \
     product_priv-app_DMService_lib_arm_libdmengine_so \
     product_priv-app_DMService_lib_arm_libdmjavaplugin_so \
     product_priv-app_DMService_lib_arm64_libdmengine_so \
-    product_priv-app_DMService_lib_arm64_libdmjavaplugin_so \
-    system_ext_priv-app_HbmSVManager_lib_arm64_libhbmsvmanager_jni_so
+    product_priv-app_DMService_lib_arm64_libdmjavaplugin_so
 
 PRODUCT_COPY_FILES += \
     vendor/google/panther/proprietary/vendor/etc/Khronos/OpenCL/vendors/ARM.icd:$(TARGET_COPY_OUT_VENDOR)/etc/Khronos/OpenCL/vendors/ARM.icd \
